a3de09140051b82084fdeb592e07893784b7b904
David Rullán Díaz <drullan.github@gmail.com>
2022-04-25 05:00:09 +0200

    =Adding all the next

--- src/Interface/OfferInterface.php
+++ src/Interface/OfferInterface.php
@@ -5,7 +5,11 @@ namespace App\Interface;
 /**
 * Interface of Data Transfer Object, that represents external JSON data
 */
-interface OfferInterface {
+interface OfferInterface
+{
+
     public function getPrice(): float;
+
     public function getVendorId(): int;
+    
 }
